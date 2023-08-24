db34d955c239e77b3c45d637e448b6113695bfe3
Karl <2147744928@qq.com>
2023-08-24 15:12:27 +0800

    :white_check_mark: 动态代理测试

--- baizhi_Spring_02/src/main/java/com/baizhi/dynamic/Before.java
+++ baizhi_Spring_02/src/main/java/com/baizhi/dynamic/Before.java
@@ -8,10 +8,14 @@ import java.lang.reflect.Method;
 public class Before implements MethodBeforeAdvice {
     /*
     作用：需要把运行在原始方法执行之前运行的额外功能，书写在before方法中
+    参数详解：
+            Method:指的是额外功能所增加的原始方法
+            Object[]:原始方法的所有参数，也就是method对应的参数
+            Object：代表原始对象（method对应的原始对象）
      */
     @Override
     public void before(Method method, Object[] objects, Object o) throws Throwable {
-        System.out.println("执行方法："+method+"，"+method.getName());
+        System.out.println("Before:执行方法："+method+"，"+method.getName());
     }
 
 }
--- baizhi_Spring_02/src/test/java/com/baizhi/TestProxy.java
+++ baizhi_Spring_02/src/test/java/com/baizhi/TestProxy.java
@@ -1,6 +1,7 @@
 package com.baizhi;
 
 import com.baizhi.proxy.*;
+import com.baizhi.proxy.a.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -27,7 +28,18 @@ public class TestProxy {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = (UserService) ctx.getBean("userService");
 
-        userService.login("suns", "123456");
+        boolean result = userService.login("suns", "123456");
+//        System.out.println("result = " + result);
         userService.register(new User());
+    }
+
+    /**
+     * 用于测试 OrderService的动态代理
+     */
+    @Test
+    public void test3() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        OrderService orderService = (OrderService) ctx.getBean("orderService");
+        orderService.showOrder();
     }
 }
